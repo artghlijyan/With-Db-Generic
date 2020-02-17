0852fc611969ff7574c6c33f66b1d022f18a2c81
Art <arthur.ghlijyan@gmail.com>
2020-02-18 01:26:33 +0400

    _

--- DbConsoleApp/GlobalObjects/Models/Student.cs
+++ DbConsoleApp/GlobalObjects/Models/Student.cs
@@ -5,7 +5,6 @@ namespace DbConsoleApp.GlobalObjects.Models
     [TableName("Student")]
     public class Student
     {
-        [Key]
         public int Id { get; set; }
 
         public string FirstName { get; set; }
