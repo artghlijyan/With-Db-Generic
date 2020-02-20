bc9031cdd103901cc11c4472a5b411c01545f4cc
Art <deckuser@hotmail.com>
2020-02-20 13:08:59 +0400

    _

--- DbFrameWork/Repasitories/DbRepasitory/DbRepasitory.cs
+++ DbFrameWork/Repasitories/DbRepasitory/DbRepasitory.cs
@@ -138,48 +138,6 @@ namespace DbFramework.Repasitories.DbRepasitory
                 return propertiesAndValues;
             }
         }
-
-
         #endregion
-
-        //public static T ToModel<T>(this IDataReader dataReader) where T : class, new()
-        //{
-        //    Type type = typeof(T);
-
-        //    var members = type
-        //        .GetProperties()
-        //        .Where(p => p.GetCustomAttribute<IgnoreAttribute>() == null)
-        //        .ToList();
-
-        //    var source = new T();
-
-        //    for (int i = 0; i < members.Count; i++)
-        //    {
-        //        if (members[i].GetCustomAttribute<DateAttribute>() != null)
-        //        {
-        //            if (DateTime.TryParse(dataReader.GetValue(i).ToString(), out DateTime date))
-        //            {
-        //                members[i].SetValue(source, date);
-        //            }
-
-        //        }
-        //        else
-        //        {
-
-        //            if (!dataReader.IsDBNull(i))
-        //            {
-        //                members[i].SetValue(source, dataReader.GetValue(i));
-        //            }
-        //            else
-        //            {
-        //                members[i].SetValue(source, null);
-        //            }
-        //        }
-
-        //    }
-
-        //    return source;
-        //}
-
     }
 }
